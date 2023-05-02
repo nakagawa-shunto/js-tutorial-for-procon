bbb4e771effef6c3fea92c8d50c0c0e48d774030
nakagawa-shunto <131745784+nakagawa-shunto@users.noreply.github.com>
2023-05-02 16:24:50 +0900

    放物線を描く公専

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let oyamaLogo = new CanvasComponents({
   position: new Vector2(GameArea.x / 2, GameArea.y / 2),
 });
 oyamaLogo.update= function () {
-  this.rotate.y +=10;
+  this.rotate -= 10;
   this.position.x += 5;
   this.position.y +=3;
   this.motion.y +=2;
